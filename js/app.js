9c11f459256879806b49688687fd1afecc154cad
codepenru01 <codepenru01@gmail.com>
2023-01-30 00:33:39 -0800

    photos

---
+++ js/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", getPost);
+let con = 0;
+let div = document.querySelector("#cardDiv");
+
+function getPost() {
+    fetch(`https://jsonplaceholder.typicode.com/photos`)
+        .then((res) => {
+            return res.json()
+        })
+        .then((post) => {
+            for (let index = 0; index < 1; index++) {
+                div.innerHTML = `
+                    <div class="card col-3 m-1 mx-auto">
+                        <img class="card-img-top" src="${post[con].thumbnailUrl}">
+                        <div class="card-body">
+                            <h5 class="card-title">${post[con].id}</h5>
+                            <p class="card-text">${post[con].title}</p>
+                        </div>
+                    </div>
+                `
+                con = con + 1;
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
